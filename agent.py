b0e5f3235aa7ecd5498bbcaca114797b03e49479
Zé Mendes <mendes.j@ua.pt>
2023-10-20 23:40:09 +0100

    Tried to make tree search to find the fastest way to coordinates. Did not try gameplay with it yet

--- agent.py
+++ agent.py
@@ -1,3 +1,5 @@
+from queue import PriorityQueue
+
 class Agent():
     def __init__(self):
         self.state = None
@@ -6,8 +8,15 @@ class Agent():
         self.closest_enemy = None
         self.map = None
         self.key = " "
+        
+
+        self.count = 0
+        self.rocks = []
+        self.offlimits = set()
+        self.trace_back = []
 
     def update_state(self, state):
+        ## go after and shoot solution
         if 'map' in state:
             print("Map received")
             self.map = state['map']
@@ -19,6 +28,9 @@ class Agent():
             self.enemy_positions = []
             for enemy in state["enemies"]:
                 self.enemy_positions.append(enemy["pos"])
+
+            for rock in state["rocks"]:
+                self.rocks.append(rock["pos"])
 
             # see which one is the closest
             self.closest_enemy = self.get_closest()
@@ -34,6 +46,59 @@ class Agent():
             self.key = " "   
 
         return self.key
+
+        # ## semi inteligent solution
+        # if 'map' in state:
+        #     self.map = state['map']
+
+        # if 'digdug' in state:
+        #     if self.offlimits == set():
+        #         self.offlimits = self.get_offlimits(state)
+
+        #     self.state = state
+        #     self.my_position = state['digdug']
+
+        #     if self.is_pooka_traversing(state):
+        #         position = self.go_back()
+        #         if self.my_position[0] - position[0] == 1:
+        #             return "a"
+        #         elif self.my_position[0] - position[0] == -1:
+        #             return "d"
+        #         elif self.my_position[1] - position[1] == 1:
+        #             return "w"
+        #         elif self.my_position[1] - position[1] == -1:
+        #             return "s"
+        #         return " "
+            
+        #     start, end = self.closest_pooka_tunnel(state)
+        #     if self.distance(self.my_position, start) < self.distance(self.my_position, end):
+        #         if self.my_position == start:
+        #             if self.wait_to_attack(self.closest_pooka(state)):
+        #                 return " "
+        #             else:
+        #                 return self.go_after_and_shoot(self.closest_pooka(state))
+                    
+        #         self.key = self.direction_to_enemy(self.my_position, start)
+        #         self.update_map()
+        #     else:
+        #         if self.my_position == end:
+        #             if self.wait_to_attack(self.closest_pooka(state)):
+        #                 return " "
+        #             else:
+        #                 return self.go_after_and_shoot(self.closest_pooka(state))
+                    
+        #         self.key = self.direction_to_enemy(self.my_position, end)
+        #         self.update_map()
+
+        # else:
+        #     self.key = " "
+
+        # return self.key
+            
+
+
+                
+            
     
     # funtion to calculate the distance between two points
     def distance(self, a, b):
@@ -114,3 +179,160 @@ class Agent():
     def update_map(self):
         if self.map != None:
             self.map[self.my_position[0]][self.my_position[1]] = 0
+            self.trace_back.append(self.my_position)
+
+
+    # function that gets the offlimits around the fygar tunnels
+    def fygar_offlimits(self, orientation, position):
+        offlimits = set()
+        x = position[0]
+        y = position[1]
+
+        if orientation == 0: # vertical
+            column = self.map[x]
+            for i in range(y, len(column)):
+                if column[i] == 0:
+                    offlimits.add((x, i), (x, i + 1), (x, i - 1), (x + 1, i), (x - 1, i))
+                else:
+                    break
+            for i in range(y, -1, -1):
+                if column[i] == 0:
+                    offlimits.add((x, i), (x, i + 1), (x, i - 1), (x + 1, i), (x - 1, i))
+                else:
+                    break
+
+        elif orientation == 1: # horizontal
+            for i in range(x, len(self.map)):
+                if self.map[i][y] == 0:
+                    offlimits.add((i, y), (i + 1, y), (i - 1, y), (i, y + 1), (i, y - 1))
+                else:
+                    break
+            for i in range(x, -1, -1):
+                if self.map[i][y] == 0:
+                    offlimits.add((i, y), (i + 1, y), (i - 1, y), (i, y + 1), (i, y - 1))
+                else:
+                    break
+
+        return offlimits
+    
+    # function that gets map when it is received and gets important information from it
+    def get_offlimits(self, state):
+        # get all fygar tunnels
+        enemies = state["enemies"]
+        offlimits = set()
+        for enemy in enemies:
+            if enemy["name"] == "fygar":
+                # see if the tunnel is horizontal or vertical
+                if enemy["dir"] == 0 or enemy["dir"] == 2: # vertical
+                    fygar = self.fygar_offlimits(0, enemy["pos"])
+                    offlimits = offlimits.union(fygar)
+                elif enemy["dir"] == 1 or enemy["dir"] == 3: # horizontal
+                    fygar = self.fygar_offlimits(1, enemy["pos"])
+                    offlimits = offlimits.union(fygar)
+                    
+        # get all rocks
+        for rock in state["rocks"]:
+            offlimits.union(rock["pos"])
+
+        return offlimits
+            
+    # function to know if a pooka is traverssing
+    def is_pooka_traversing(self, state):
+        enemies = state["enemies"]
+        for enemy in enemies:
+            if enemy["name"] == "pooka":
+                if enemy["state"] == True:
+                    return True
+        return False
+    
+    # function to go back
+    def go_back(self):
+        if self.trace_back != []:
+            return self.trace_back.pop()
+        else:
+            return None
+        
+    def closest_pooka(self, state):
+        pookas = [p for p in state["enemies"] if p["name"] == "Pooka"]
+        if pookas == []:
+            return None
+        
+        closest = pookas[0]
+        for pooka in pookas:
+            if self.distance(self.my_position, pooka["pos"]) < self.distance(self.my_position, closest["pos"]):
+                closest = pooka
+        return closest
+    
+    def closest_pooka_tunnel(self, state): # will return the start and end of the tunnel
+        pooka = self.closest_pooka(state)
+        start = pooka["pos"]
+        end = pooka["pos"]
+
+        if pooka["dir"] == 0 or pooka["dir"] == 2: # vertical
+            column = self.map[pooka["pos"][0]]
+            for i in range(pooka["pos"][1], len(column)):
+                if column[i] == 0:
+                    end = (pooka["pos"][1], i)
+                else:
+                    break
+
+            for i in range(pooka["pos"][1], -1, -1):
+                if column[i] == 0:
+                    start = (pooka["pos"][1], i)
+                else:
+                    break
+
+            start = (pooka["pos"][0], start[1] - 2) # to go to the start of the tunnel without making a hole yet
+            end = (pooka["pos"][0], end[1] + 2) # same here
+
+        elif pooka["dir"] == 1 or pooka["dir"] == 3: # horizontal
+            for i in range(pooka["pos"][0], len(self.map)):
+                if self.map[i][pooka["pos"][1]] == 0:
+                    end = (i, pooka["pos"][1])
+                else:
+                    break
+
+            for i in range(pooka["pos"][0], -1, -1):
+                if self.map[i][pooka["pos"][1]] == 0:
+                    start = (i, pooka["pos"][1])
+                else:
+                    break
+
+            start = (start[0] - 2, pooka["pos"][1]) # to go to the start of the tunnel without making a hole yet
+            end = (end[0] + 2, pooka["pos"][1]) # same here
+                
+
+        return (start, end)
+        
+
+    def wait_to_attack(self, pooka):
+        # vertical
+        if pooka["dir"] == 0 and self.my_position[1] < pooka["pos"][1]:
+            return True
+        elif pooka["dir"] == 2 and self.my_position[1] > pooka["pos"][1]:
+            return True
+
+        # horizontal
+        elif pooka["dir"] == 1 and self.my_position[0] > pooka["pos"][0]:
+            return True
+        elif pooka["dir"] == 3 and self.my_position[0] < pooka["pos"][0]:
+            return True
+            
+        return False
+    
+    def go_after_and_shoot(self, pooka):
+        # vertical
+        if pooka["dir"] == 0 and (pooka["pos"][1] - self.my_position[1]) > 3:
+            return "w"
+        elif pooka["dir"] == 2 and (self.my_position[1] - pooka["pos"][1]) > 3:
+            return "s"
+        
+        # horizontal
+        elif pooka["dir"] == 1 and (pooka["pos"][0] - self.my_position[0]) > 3:
+            return "d"
+        elif pooka["dir"] == 3 and (self.my_position[0] - pooka["pos"][0]) > 3:
+            return "a"
+        
+        return "A"
+        
+    #def pooka_in_my_tunel(self, p)
---
+++ tree_search.py
@@ -0,0 +1,196 @@
+# Module: tree_search
+#
+# This module provides a set o classes for automated
+# problem solving through tree search:
+#    SearchDomain  - problem domains
+#    SearchProblem - concrete problems to be solved
+#    SearchNode    - search tree nodes
+#    SearchTree    - search tree with the necessary methods for searhing
+#
+#  (c) Luis Seabra Lopes
+#  Introducao a Inteligencia Artificial, 2012-2019,
+#  Inteligência Artificial, 2014-2019
+
+from abc import ABC, abstractmethod
+
+
+# Dominios de pesquisa
+# Permitem calcular
+# as accoes possiveis em cada estado, etc
+class SearchDomain(ABC):
+    # construtor
+    @abstractmethod
+    def __init__(self):
+        pass
+
+    # lista de accoes possiveis num estado
+    @abstractmethod
+    def actions(self, state):
+        pass
+
+    # resultado de uma accao num estado, ou seja, o estado seguinte
+    @abstractmethod
+    def result(self, state, action):
+        pass
+
+    # custo de uma accao num estado
+    @abstractmethod
+    def cost(self, state, action):
+        pass
+
+    # custo estimado de chegar de um estado a outro
+    @abstractmethod
+    def heuristic(self, state, goal):
+        pass
+
+    # test if the given "goal" is satisfied in "state"
+    @abstractmethod
+    def satisfies(self, state, goal):
+        pass
+
+
+# Problemas concretos a resolver
+# dentro de um determinado dominio
+class SearchProblem:
+    def __init__(self, domain, initial, goal):
+        self.domain = domain
+        self.initial = initial
+        self.goal = goal
+
+    def goal_test(self, state):
+        return self.domain.satisfies(state, self.goal)
+
+
+# Nos de uma arvore de pesquisa
+class SearchNode:
+    def __init__(self, state, parent, depth=0, cost=0, heuristic=None, action=None):
+        self.state = state
+        self.parent = parent
+        self.depth = depth  # adicionado para calcular a profundidade
+        self.cost = cost  # adicionado para calcular o custo
+        self.heuristic = heuristic  # 12
+        self.action = action  # strips
+
+    def in_parent(self, newState):  # adicionado para evitar ciclos
+        if self.parent == None:
+            return False
+        if self.parent.state == newState:
+            return True
+        return self.parent.in_parent(newState)
+
+    def __str__(self):
+        return "no(" + str(self.state) + "," + str(self.parent) + ")"
+
+    def __repr__(self):
+        return str(self)
+
+    def plan(self):
+        return self.parent.plan() + [self.action] if self.parent else []
+
+
+# Arvores de pesquisa
+class SearchTree:
+    # construtor
+    def __init__(self, problem, strategy="breadth"):
+        self.problem = problem
+        root = SearchNode(
+            problem.initial,
+            None,
+            heuristic=problem.domain.heuristic(problem.initial, problem.goal),
+        )  # 12
+        self.open_nodes = [root]
+        self.strategy = strategy
+        self.solution = None
+        self.non_terminals = 0  # adicionado para calcular o numero de nos nao terminais
+        self.highest_cost_nodes = [root]  # 15 tem a root pois e o no com maior custo
+        self.total_depth = 0  # 16
+
+    @property  # adicionado para calcular o comprimento da solucao
+    def length(self):
+        return self.solution.depth
+
+    @property  # adicionado para calcular o numero de nos terminais
+    def terminals(self):
+        return len(self.open_nodes) + 1
+
+    @property
+    def avg_branching(self):
+        return (self.terminals + self.non_terminals - 1) / self.non_terminals
+
+    @property  # ex9
+    def cost(self):
+        return self.solution.cost
+
+    @property  # strips
+    def plan(self):
+        return self.solution.plan()
+
+    @property  # 16
+    def average_depth(self):
+        return self.total_depth / (self.terminals + self.non_terminals)
+
+    # obter o caminho (sequencia de estados) da raiz ate um no
+    def get_path(self, node):
+        if node.parent == None:
+            return [node.state]
+        path = self.get_path(node.parent)
+        path += [node.state]
+        return path
+
+    # procurar a solucao
+    def search(self, limit=None):
+        while self.open_nodes != []:
+            node = self.open_nodes.pop(0)
+            if self.problem.goal_test(node.state):
+                self.solution = node
+                return self.get_path(node)
+            self.non_terminals += (
+                1  # adicionado para calcular o numero de nos nao terminais
+            )
+            lnewnodes = []
+            for a in self.problem.domain.actions(node.state):
+                newstate = self.problem.domain.result(node.state, a)
+                if not node.in_parent(newstate):
+                    if (
+                        limit is None or node.depth < limit
+                    ):  # adicionado para limitar a profundidade
+                        newnode = SearchNode(
+                            newstate,
+                            node,
+                            node.depth + 1,
+                            node.cost + self.problem.domain.cost(node.state, a),  # ex8
+                            self.problem.domain.heuristic(
+                                newstate, self.problem.goal
+                            ),  # 12
+                            a,  # strips
+                        )  # adicionado para calcular a profundidade
+                        if self.highest_cost_nodes[0].cost < newnode.cost:  # 15
+                            self.highest_cost_nodes = [newnode]
+                        elif self.highest_cost_nodes[0].cost == newnode.cost:
+                            self.highest_cost_nodes.append(newnode)
+
+                        self.total_depth += newnode.depth  # 16
+                        
+                        # print(newnode.state["digdug"])
+                        lnewnodes.append(newnode)
+            self.add_to_open(lnewnodes)
+        return None
+
+    # juntar novos nos a lista de nos abertos de acordo com a estrategia
+    def add_to_open(self, lnewnodes):
+        if self.strategy == "breadth":
+            self.open_nodes.extend(lnewnodes)
+        elif self.strategy == "depth":
+            self.open_nodes[:0] = lnewnodes
+            self
+        elif self.strategy == "uniform":  # ex10
+            self.open_nodes.extend(lnewnodes)  # adicionado os novos nos
+            self.open_nodes.sort(key=lambda node: node.cost)  # orderna os nos por custo
+
+        elif self.strategy == "greedy":  # 13
+            self.open_nodes.extend(lnewnodes)
+            self.open_nodes.sort(key=lambda node: node.heuristic)
+
+        elif self.strategy == "a*":  # 14
+            self.open_nodes.extend(lnewnodes)
+            self.open_nodes.sort(key=lambda node: node.cost + node.heuristic)
